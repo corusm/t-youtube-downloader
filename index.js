4381ddf1bfcb503c282cdcaf2730d54a32bac2bd
Niklas <dev@corusm.de>
2018-12-26 13:22:18 +0100

    bug fixes (videos over 5min length couldnt be send)

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// Modules
 const Telegraf = require('telegraf');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const path = require('path');
 
+// Files
+const config = require(`./config.json`);
+
+// Variables
+var vidID = 1;
+var downurl = "https://www.youtube.com/watch?v=";
+const directory = `${__dirname}/cache`;
+
+// --------
 // DOC
 // add bot token in "config.json"
 // {
@@ -10,36 +21,68 @@ const ytdl = require('ytdl-core');
 // npm install ytdl-core --save
 // node index.js
 
-const config = require(`./config.json`);
-
-console.log("Bot is running;; TOKEN: " + config.token);
-
-var downurl = "https://www.youtube.com/watch?v=";
-
+// Bot setup
 const bot = new Telegraf(config.token);
+console.log("Bot is running;; TOKEN: " + config.token);
 bot.start((ctx) => ctx.reply('Hey there!'));
-bot.help((ctx) => ctx.reply('Send me a link and I will send you the vid :) \n cmds: \n \n /video {videoID} -- is downloading the video \n /get -- is sending the video'));
+bot.help((ctx) => ctx.reply('Send me a link and I will send you the vid :) \n cmds: \n \n /video {videoID} -- is downloading the video \n /get -- is sending the video \n /clear -- clears all videos from cache'));
 bot.startPolling();
 
 // Download Video to Server
 bot.command('/video', (ctx) => {
+    videoId();
     let input = ctx.message["text"];
     let subText = input.split(" ");
     let out = downurl + subText[1];
     console.log(out);
-    ytdl(out)
-        .pipe(fs.createWriteStream(`${__dirname}/video.mp4`))
-    ctx.reply('Video has been downloaded!');
+    ytdl(out, "lowest", "videoonly")
+        .pipe(fs.createWriteStream(`${__dirname}/cache/${vidID}.flv`))
+    ctx.reply(`Video has been downloaded! SavingID = ${vidID}`);
+
+    if (vidID > 9) {
+        clearStorage();
+        ctx.reply('Server storage cleared!')
+    }
 })
 
 // Send video to User
 bot.command('/get', (ctx) => {
     ctx.reply('Sending video...');
     try {
+        console.log("Sending video...");
         ctx.replyWithVideo({
-            source: fs.createReadStream(`${__dirname}/video.mp4`)
+            source: fs.createReadStream(`${__dirname}/cache/${vidID}.flv`)
         })
-    } catch(err) {
-        ctx.reply("Error (Video to long?)");
+    } catch (err) {
+        ctx.reply("Error");
     }
 })
+
+// Clear videos from cache
+bot.command('/clear', (ctx) => {
+    clearStorage();
+    ctx.reply('All videos removed');
+})
+
+// Functions
+
+// Count videoId++
+function videoId() {
+    return vidID++;
+}
+
+// Clear Cache
+function clearStorage() {
+    fs.readdir(directory, (err, files) => {
+        if (err) console.log(err);
+
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) console.log(err);
+            });
+        }
+    });
+}
+
+// Catch all errors from bot
+bot.catch(function (error) { console.log(err) });
